aad9059763918a8b810abd93f6ad5f8eb926948a
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2024-07-14 07:41:06 +0100

    Try another abi
    Signed-off-by: Ilya Savitsky <ilya.savitsky@codethink.co.uk>

--- scripts/calculate_address.py
+++ scripts/calculate_address.py
@@ -1,8 +1,11 @@
 import sys
 from web3.utils.address import get_create2_address
+from Crypto.Hash import keccak
 import json
 
 if __name__ == "__main__":
     data = json.loads(sys.argv[1])
-    addr = get_create2_address(data["sender"], data["salt"], data["init_code"])
+    keccak_hash = keccak.new(digest_bits=256)
+    keccak_hash.update(data["init_code"].encode())
+    addr = get_create2_address(data["sender"].encode(), data["salt"].encode(), keccak_hash.hexdigest())
     print(json.dumps({"address": addr}))
